46bcbff5a926aeca5966dabafcc9519653e87449
Ben Webb <ben@salilab.org>
2021-06-15 10:08:21 -0700

    Try to avoid memory exhaustion on Linux ARM

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,19 @@ export LANG=en_US.UTF-8
 # Don't build the scratch or cnmultifit modules
 DISABLED=scratch:cnmultifit
 
+# Avoid running out of memory on ARM by splitting up IMP.cgal
+if [ `uname -m` = "aarch64" ]; then
+  PERCPPCOMP="-DIMP_PER_CPP_COMPILATION=cgal"
+else
+  PERCPPCOMP=""
+fi
+
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=${DISABLED} \
       -G Ninja \
       -DIMP_USE_SYSTEM_RMF=on \
       -DIMP_USE_SYSTEM_IHM=on \
-      ${CMAKE_ARGS} \
+      ${CMAKE_ARGS} ${PERCPPCOMP} \
       -DUSE_PYTHON2=off \
       -DPython3_FIND_FRAMEWORK=NEVER \
       ..
