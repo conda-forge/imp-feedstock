90ad0f593d2c9c700c790ce1e55e350e34b15c0d
Ben Webb <ben@salilab.org>
2023-06-22 16:02:11 -0700

    Use all cores on Mac
    Mac build hosts don't seem to have problems with
    running out of memory, unlike some of the Linux
    machines.

--- recipe/build.sh
+++ recipe/build.sh
@@ -26,10 +26,14 @@ cmake -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=${DISABLED} \
 # in the final package, but quicker to abort here if they're missing)
 python "${RECIPE_DIR}/check_disabled_modules.py" ${DISABLED} || exit 1
 
-# On some platforms (notably aarch64 with Drone) builds can fail due to
-# running out of memory. If this happens, try the build again; if it
-# still fails, restrict to one core.
-ninja install -j 2 -k 0 || ninja install -j 2 -k 0 || ninja install -j 1
+if [ `uname -s` = "Darwin" ]; then
+  ninja install
+else
+  # On some Linux platforms (notably aarch64 with Drone) builds can fail due to
+  # running out of memory. If this happens, try the build again; if it
+  # still fails, restrict to one core.
+  ninja install -j 2 -k 0 || ninja install -j 2 -k 0 || ninja install -j 1
+fi
 
 # Don't distribute example application
 rm -f ${PREFIX}/bin/imp_example_app
